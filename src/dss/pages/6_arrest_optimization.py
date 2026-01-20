ee500d3776d115daa5fec772ea0503c4ad9ef310
quintenzondern <damquinten@gmail.com>
2026-01-20 12:39:50 +0100

    Update community method options in sidebar

--- src/dss/pages/6_arrest_optimization.py
+++ src/dss/pages/6_arrest_optimization.py
@@ -108,10 +108,10 @@ def page() -> None:
 
     # Sidebar parameters
     st.sidebar.header("Optimisation parameters")
-    comm_method_labels = {
-                "Louvain": "louvain",
+    comm_method_labels = {"Spectral": "spectral",
                 "Girvan Newman": "girvan_newman",
-                "Spectral": "spectral"}
+                "Louvain": "louvain"
+                }
     comm_label = st.sidebar.selectbox("Community method", list(comm_method_labels.keys()), index=0, help ="Select the method that detects the communities in the network.")
     comm_method = comm_method_labels[comm_label]
     alpha = st.sidebar.slider("Importance Communities (alpha)", 0.0, 5.0, 1.0, 0.1,help="How important it is to keep members of the same community together. Higher values make the model more likely to keep them together.")
