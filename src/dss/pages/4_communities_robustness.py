fea2c6b296dddd6c4e913bce1ec05979633afb19
quintenzondern <damquinten@gmail.com>
2026-01-21 20:26:46 +0100

    Refactor help texts for community robustness analysis

--- src/dss/pages/4_communities_robustness.py
+++ src/dss/pages/4_communities_robustness.py
@@ -142,17 +142,20 @@ Selected nodes will:
     # Display summary
     col_stats, col_plot = st.columns(2)
     with col_stats:
-        st.subheader("Community summary", help= """
+        st.subheader("Community summary", 
+                     help= """
 Size = Amount of nodes in cluster
 Within Ratio = A measure of how internally connected the communities are, as oposed to connections outside of the community. 
 High within ratio: Community mostly communicates within the community. 
 Low within ratio: Community interacts heavily with other communities.
-""")
-        st.write(f"Modularity Q: {comm_result.modularity:.3f}", help="""
+"""
+                    )
+        st.write(f"Modularity Q: {comm_result.modularity:.3f}", 
+                 help="""
 A measure of how well a network is partitioned into communities. 
 Value close to 1: indicates strong community structure. 
 Value close to 0: indicates weak community structure.
-        """
+"""
         )
         st.dataframe(comm_result.summary)
         
@@ -162,7 +165,8 @@ Value close to 0: indicates weak community structure.
         st.subheader("Network coloured by communities", help= """
 Visualisation of clustered network with selected method and parameters.
 Different colors represent different communities.
-        """)   
+"""
+                    )   
         display_network(
             G,
             node_color=community_colors,
@@ -185,22 +189,28 @@ Different colors represent different communities.
             df_details["role"] = [role_result.labels[n] for n in selected_nodes]
         st.dataframe(df_details)
         
-    st.subheader("Robustness analysis", help= """
-    Robustness analysis evaluates how stable the results of a network analysis are when the network is slightly altered or when different methods are applied. 
-    A robust result indicates that the identified structure reflects meaningful patterns rather than noise or modeling choices.
-    """)
+    st.subheader("Robustness analysis", 
+                 help= """
+Robustness analysis evaluates how stable the results of a network analysis are when the network is slightly altered or when different methods are applied. 
+A robust result indicates that the identified structure reflects meaningful patterns rather than noise or modeling choices.
+"""
+                )
     st.write("Click on Run Robustness Test in the side bar to (re)run robustness test")
     if robustness_result is not None:
-        st.write(f"Average ARI across runs: {sum(robustness_result.ari_scores) / len(robustness_result.ari_scores):.3f}", help= """
+        st.write(f"Average ARI across runs: {sum(robustness_result.ari_scores) / len(robustness_result.ari_scores):.3f}", 
+                 help= """
 Measures the similarity between two clusterings while correcting for simularities that could occur by chance. 
 In this context, it is used to quantify how consistently communities are identified under network perturbations. 
         
 ARI close to 1 = Robust method of community clustering. 
 ARI close to 0 = Community completely changes when perturbation test is applied
-        """)
-        st.write(f"Average modularity drop: {sum(robustness_result.modularity_drops) / len(robustness_result.modularity_drops):.3f}", help= """
+"""
+                )
+        st.write(f"Average modularity drop: {sum(robustness_result.modularity_drops) / len(robustness_result.modularity_drops):.3f}", 
+                 help= """
 Average drop in modularity when perturbation test is applied.
-        """)
+"""
+                )
         col_hist, col_box = st.columns(2)
 
         with col_hist:
